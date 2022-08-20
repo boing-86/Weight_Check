d06e4b4712347a226bccc1e4d338eb1e34ec7de1
hyunwoo0081 <hyunwoo0081@gmail.com>
2022-08-20 23:25:00 +0900

    backend /weight 라우팅 설정 및 기능 추가 database 설정, database string 형식 결정 후 재설계 예정

--- WEB(backend)/app.py
+++ WEB(backend)/app.py
@@ -1,27 +1,60 @@
 from flask import Flask, request, jsonify
-from functools import reduce
+import pymysql
 import math
 
+# mysql = MySQL()
 app = Flask(__name__)
-
+# todo: mysql 연결 설정 하기
+mysql = pymysql.connect(host = '',
+                        user='',
+                        password='',
+                        database='',
+                        cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def hello_world():
     return 'Hello Backend World!'
 
 @app.route('/weight', methods=['POST'])
-def get():
-    try:
-        print(request.form['id'])
-    except:
-        print("error")
-        
-    product_mean = [1, 2, 3, 4]
-    product_std = [1, 1, 1, 1]
-    mean = sum(product_mean)
-    std = math.sqrt(reduce(lambda s, x: s + x ** 2, product_std))
+def get_weight_sum():
+    mean = std = 0
+    barcode_id = request.form['id']
+
+    # 바코드가 피킹/DAS 에서 쓰이는지 확인
+    # todo: 바코드 임의로 정하면, 문자열 파싱해서 알 수 있음
+    is_picking_zone = True # 임의 값
+
+    # DB 에서 상품 리스트 찾기
+    with mysql.cursor() as cursor:
+        if is_picking_zone:
+            cursor.execute("sql: picking_id == %s" %barcode_id)
+            data = cursor.fatchone()
+            
+            product_names = [ data['product_id'] ]
+            product_counts= [ data['p_product_count'] ]
+        else:
+            cursor.execute("sql: das_id == %s" %barcode_id)
+            data = cursor.fatchone()
+            
+            # todo: 쿼리 문자열 파싱 (string -> List)
+            product_names = data['']
+            product_counts= data['']
     
+    # DB 에서 상품 평균, 표준 편차 구하기
+    with mysql.cursor() as cursor:
+        for name, count in zip(product_names, product_counts):
+            cursor.execute("sql: product_id == %s" %name)
+            product = cursor.fatchone()
+            
+            mean += count * product['p_weight_avg']
+            std  += count *(product['p_weight_std']**2)
+    std = math.sqrt(std)
+
     return jsonify({'mean': mean, 'std': std})
 
+@app.errorhandler(404)
+def error404():
+    return "Not Found"
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=6000, debug=True)
+    app.run(host='0.0.0.0', port=6000, debug=True)
