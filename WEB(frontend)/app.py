b975e3f82f96f109a48f512d311fac078361ab5d
MinseonKang <s_2___@naver.com>
2022-08-20 17:27:17 +0900

    frontend setting

--- WEB(frontend)/app.py
+++ WEB(frontend)/app.py
@@ -1,12 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def home():
+   return 'This is Home!'
 
 if __name__ == '__main__':
-    app.run()
+   app.run('0.0.0.0',port=5000,debug=True)
