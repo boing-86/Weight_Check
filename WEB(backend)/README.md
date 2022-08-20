92d9cc18649f0162832684a6bbf98f981113b43b
hyunwoo0081 <hyunwoo0081@gmail.com>
2022-08-20 19:14:07 +0900

    backend weight 기능 일부 추가

---
+++ WEB(backend)/README.md
@@ -0,0 +1,2 @@
+#Weight_Check Backend Server
+백엔드 서버입니다
--- WEB(backend)/app.py
+++ WEB(backend)/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from functools import reduce
+import math
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello Backend World!'
 
+@app.route('/weight', methods=['POST'])
+def get():
+    try:
+        print(request.form['id'])
+    except:
+        print("error")
+        
+    product_mean = [1, 2, 3, 4]
+    product_std = [1, 1, 1, 1]
+    mean = sum(product_mean)
+    std = math.sqrt(reduce(lambda s, x: s + x ** 2, product_std))
+    
+    return jsonify({'mean': mean, 'std': std})
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port=6000, debug=True)
