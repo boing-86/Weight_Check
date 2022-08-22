912f749ff37f93f61f4414604af8dd67e8f099f1
MinseonKang <s_2___@naver.com>
2022-08-22 16:54:35 +0900

    index 설정

--- WEB(frontend)/app.py
+++ WEB(frontend)/app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)       # Flask라는 이름의 객체 생성
 # GPIO.cleanup()
 
 @app.route('/')                       # 기본 주소
-def home():
-
-    return render_template()
+def index():
+    return render_template('index.html')
     #index2.html에 스위치의 눌림 여부 현황을 전달
 
 if __name__ == "__main__":  # 웹사이트를 호스팅하여 접속자에게 보여주기 위한 부분
